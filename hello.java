2c3db5c4cb9e83d4c9c7ae885bae4e3a7b7ee2d9
ddgmma <yangyang2631@gmail.com>
2026-05-13 15:36:44 +0800

    添加readme文件

--- hello.java
+++ hello.java
@@ -1,6 +1,44 @@
-public class hello {
+
+// 输出空心菱形
+import java.util.Scanner;
+
+public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("你好世界");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("输入菱形的高度：");
+        int n = scanner.nextInt();
+
+        // 上半部分
+        for (int i = 1; i <= n; i++) {
+            for (int k = (n - i); k >= 0; k--) {
+                System.out.print(" ");
+            }
+            for (int x = 1; x <= (2 * i - 1); x++) {
+                if (i == 1 || i == n || x == 1 || x == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        // 下半部分
+        for (int i = (n - 1); i >= 1; i--) {
+            for (int k = 0; k <= (n - i); k++) {
+                System.out.print(" ");
+            }
+            for (int x = (2 * i - 1); x >= 1; x--) {
+                if (i == 1 || i == n || x == 1 || x == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        scanner.close();
     }
 }
